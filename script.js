09cc291d64720d2d4fa6a6003e6f972a439197fc
Julia.Zorina <julia.zorinaa@gmail.com>
2021-01-14 21:44:48 +0200

    added some variants

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 console.log("js_start");
-let change = document.querySelector(".view");
-let show_link = document.querySelector(".show");
-console.log("change");
-console.log("show_link");
+// let changes = document.querySelectorAll(".view");
+// let links = document.querySelectorAll(".show");
 
-change.addEventListener("mouseover", function (event) {
-  event.preventDefault();
+let proj = document.querySelector(".item_1");
+let change = document.querySelector(".view");
+let link = document.querySelector(".show a");
+console.log("link");
+change.addEventListener("click", function () {
+
   console.log("event");
-  show_link.classList.toggle("active");
+  // change.replaceWith(link);
+  // link.classList.add("active");
+  if(!change.classList.contains("new")){
+    change.classList.add("new")&&
+    link.classList.add("active")
+  }else{
+    change.classList.remove("new")&&
+    link.classList.remove("active")
+  }
+
+  // if(!link.classList.contains("active")){
+  //   link.classList.add("active")
+  // }else{
+  //   link.classList.remove("active")
+  // }
 });
+
+
+
+
+
