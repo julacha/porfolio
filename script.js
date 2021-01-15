9ddb9c7eeec713ac62eb968619001bd16d03b463
Julia.Zorina <julia.zorinaa@gmail.com>
2021-01-15 13:52:15 +0200

    added function

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 console.log("js_start");
-// let changes = document.querySelectorAll(".view");
-// let links = document.querySelectorAll(".show");
 
-let proj = document.querySelector(".item_1");
-let change = document.querySelector(".view");
-let link = document.querySelector(".show a");
-console.log("link");
-change.addEventListener("click", function () {
-
-  console.log("event");
-  change.replaceWith(link);
-  link.classList.add("active");
-  // if(!change.classList.contains("new")){
-  //   change.classList.add("new")&&
-  //   link.classList.add("active")
-  // }else{
-  //   change.classList.remove("new")&&
-  //   link.classList.remove("active")
-  // }
-
-  // if(!link.classList.contains("active")){
-  //   link.classList.add("active")
-  // }else{
-  //   link.classList.remove("active")
-  // }
-});
+for (i = 1, j = 1; i <=6, j <= 6; i++,j++){
+  let change = document.querySelector(".view");
+  let link = document.querySelector(".show a");
+  console.log("link");
+  change.addEventListener("mouseover", function () {
+    console.log("event");
+    change.replaceWith(link);
+    link.classList.add("active");
+  });
+  }
 
 
-
-
-
+// let project_1 = document.querySelector(".item_1")
+// let change = document.querySelector(".view");
+// let link = document.querySelector(".show a");
+// console.log("link");
+// change.addEventListener("mouseover", function () {
+//   console.log("event");
+//   change.replaceWith(link);
+//   link.classList.add("active");
+// });
