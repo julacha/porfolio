adcc386086c2d7d561b430db8626ffb3a3b491ca
Julia.Zorina <julia.zorinaa@gmail.com>
2021-01-15 14:56:49 +0200

    added mouseout

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 console.log("js_start");
 
-for (i = 1, j = 1; i <=6, j <= 6; i++,j++){
+// for (let i = 1, i <=6; i++){
   let change = document.querySelector(".view");
   let link = document.querySelector(".show a");
   console.log("link");
+  //let move = 0;
   change.addEventListener("mouseover", function () {
     console.log("event");
     change.replaceWith(link);
     link.classList.add("active");
   });
-  }
-
+  link.addEventListener("mouseout", function () {
+    console.log("event");
+  //if(link.classList.contains("active")){
+    link.replaceWith(change);
+  // } else{
+  //   link.classList.add("active");
+  // }
+  });
 
 // let project_1 = document.querySelector(".item_1")
 // let change = document.querySelector(".view");
