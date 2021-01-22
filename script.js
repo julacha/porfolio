5d6d1f3bf774920f65b07ae68845846986f0907d
Julia.Zorina <julia.zorinaa@gmail.com>
2021-01-22 15:54:24 +0200

    added coment

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 console.log("js_start");
 
 // for (let i = 1, i <=6; i++){
-  // let change = document.querySelector(".view");
-  // let link = document.querySelector(".show a");
-  // console.log("link");
-  // //let move = 0;
-  // change.addEventListener("mouseover", function () {
-  //   console.log("event");
-  //   change.replaceWith(link);
+  let change = document.querySelector(".view");
+  let link = document.querySelector(".show a");
+  console.log("link");
+  //let move = 0;
+  change.addEventListener("mouseover", function () {
+    console.log("event");
+    change.replaceWith(link);
+    link.classList.add("active");
+  });
+  link.addEventListener("mouseout", function () {
+    console.log("event");
+  //if(link.classList.contains("active")){
+    link.replaceWith(change);
+  // } else{
   //   link.classList.add("active");
-  // });
-  // link.addEventListener("mouseout", function () {
-  //   console.log("event");
-  // //if(link.classList.contains("active")){
-  //   link.replaceWith(change);
-  // // } else{
-  // //   link.classList.add("active");
-  // // }
-  // });
+  // }
+  });
 
 // let project_1 = document.querySelector(".item_1")
 // let change = document.querySelector(".view");
